92d078db1f6ca7fe7f172bbc3ec3ffcba7ca73d1
Philip Penquitt <30809710+PhilipPenquitt@users.noreply.github.com>
2021-12-31 01:17:33 +0100

    Added Randomize and File Flag

--- students/phpe/main.go
+++ students/phpe/main.go
@@ -3,23 +3,30 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"runtime"
 	"strings"
+	"time"
 )
 
-// - wir wollen eine Datei via Flag öffnen
+// - wir wollen eine Datei via Flag öffnen --> erledigt
 // - diese Auslesen und die Fragen speichern --> erledigt
 // - die Fragen durchgehen und prüfen ob die Antworten übereinstimmen --> erledigt
+// Die Fragen sollen optional via Schalter per Zufall ausgegeben werden --> erledigt
+// Nach Ablauf des Timers soll ein Score aller erfolgreicher Fragen angezeigt werden
+var randomize bool
+var filepath string
 
 type Quiz struct {
-	Frage   string
-	Antwort string
+	Question string
+	Answer   string
 }
 
-func read_csv() (quizfragen []Quiz) {
-	file, err := os.Open("problems.csv")
+func read_csv(filepath string) (quizfragen []Quiz) {
+	file, err := os.Open(filepath)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -27,8 +34,8 @@ func read_csv() (quizfragen []Quiz) {
 	records, _ := reader.ReadAll()
 	for _, row := range records {
 		quiz := Quiz{
-			Frage:   row[0],
-			Antwort: strings.TrimSpace(row[1]),
+			Question: row[0],
+			Answer:   strings.TrimSpace(row[1]),
 		}
 		quizfragen = append(quizfragen, quiz)
 	}
@@ -39,22 +46,40 @@ func read_csv() (quizfragen []Quiz) {
 func abfragen(quizfragen []Quiz) {
 	reader := bufio.NewReader(os.Stdin)
 	for _, quizfrage := range quizfragen {
-		fmt.Println("Lösung zu:", quizfrage.Frage)
+		fmt.Println("Question:", quizfrage.Question)
 		antwort, _ := reader.ReadString('\n')
 		antwort = strings.Replace(antwort, "\n", "", -1)
 		// Remove \r from Answer when on windows
 		if runtime.GOOS == "windows" {
 			antwort = strings.Replace(antwort, "\r", "", -1)
 		}
-		if quizfrage.Antwort == antwort {
-			fmt.Println("stimmt")
-		} else {
-			fmt.Println("Das war falsch")
-		}
+		// We don't want this in part 2 of the Tasks
+		// if quizfrage.Answer == antwort {
+		// fmt.Println("correct")
+		// } else {
+		// fmt.Println("Thats wrong")
+		// }
 	}
 }
 
+func init() {
+	flag.BoolVar(&randomize, "randomize", false, "randomize ofer of question")
+	flag.StringVar(&filepath, "file", "problems.csv", "Path to file")
+}
+
 func main() {
-	quizfragen := read_csv()
+	flag.Parse()
+	var quizfragen []Quiz
+	if filepath != "problems.csv" {
+		quizfragen = read_csv(filepath)
+	} else {
+		quizfragen = read_csv("problems.csv")
+	}
+	if randomize {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(quizfragen), func(i, j int) {
+			quizfragen[i], quizfragen[j] = quizfragen[j], quizfragen[i]
+		})
+	}
 	abfragen(quizfragen)
 }
